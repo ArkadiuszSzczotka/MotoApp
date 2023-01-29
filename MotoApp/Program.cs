371f8dfe31dc86bc794715f58526f0d30aa67d3f
Arkadiusz <szczotka.ark@gmail.com>
2023-01-29 21:01:33 +0100

    Add project files.

---
+++ MotoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using MotoApp.Repositories;
+using MotoApp.Entities;
+
+var employeeRepository = new GenericRepository<Employee>();
+
+employeeRepository.Add(new Employee { FirstName = "Arek" });
+employeeRepository.Add(new Employee { FirstName = "Marek" });
+employeeRepository.Add(new Employee { FirstName = "Jola" });
+
+employeeRepository.Save();
+
+var empRepoWithRemove = new GenericRepositoryWithRemove<Employee>();
+empRepoWithRemove.Add(new Employee { FirstName = "Tomek" });
+empRepoWithRemove.Add(new Employee { FirstName = "Adam" });

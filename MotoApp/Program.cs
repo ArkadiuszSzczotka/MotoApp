edd29f8fe6fb55ee1dcadba3328b517b2d808e59
Arkadiusz <szczotka.ark@gmail.com>
2023-02-06 21:23:14 +0100

    EF core in memory repository pattern.

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,14 +1,32 @@
 ﻿using MotoApp.Repositories;
 using MotoApp.Entities;
+using MotoApp.Data;
 
-var employeeRepository = new GenericRepository<Employee>();
+var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
+AddEmployees(employeeRepository);
+AddManagers(employeeRepository);
+WriteToConsole(employeeRepository);
 
-employeeRepository.Add(new Employee { FirstName = "Arek" });
-employeeRepository.Add(new Employee { FirstName = "Marek" });
-employeeRepository.Add(new Employee { FirstName = "Jola" });
+static void AddEmployees(IRepository<Employee> employeeRepository)
+{
+    employeeRepository.Add(new Employee { FirstName = "Arek" });
+    employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+    employeeRepository.Add(new Employee { FirstName = "Basia" });
+    employeeRepository.Save();
+}
 
-employeeRepository.Save();
+static void AddManagers(IWriteRepository<Manager> managerRepository)
+{
+    managerRepository.Add(new Manager { FirstName = "Grzegorz" });
+    managerRepository.Add(new Manager { FirstName = "Mask" });
+    managerRepository.Save();
+}
 
-var empRepoWithRemove = new GenericRepositoryWithRemove<Employee>();
-empRepoWithRemove.Add(new Employee { FirstName = "Tomek" });
-empRepoWithRemove.Add(new Employee { FirstName = "Adam" });
+static void WriteToConsole(IReadRepository<IEntity> repository)
+{
+    var items = repository.GetAll();
+    foreach(var item in items)
+    {
+        Console.WriteLine(item);
+    }    
+}
---
+++ MotoApp/Repositories/ListRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace MotoApp.Repositories
+{
+    using MotoApp.Entities;
+
+    public class ListRepository<T> : IRepository<T> where T : class, IEntity, new()
+    {
+        protected readonly List<T> _items = new();
+
+        public IEnumerable<T> GetAll()
+        {
+            return _items.ToList();
+        }
+
+        public void Add(T item)
+        {
+            item.Id = _items.Count + 1;
+            _items.Add(item);
+        }
+
+        public T GetById(int id)
+        {
+            return _items.Single(item => item.Id == id);
+        }
+
+        public void Remove(T item)
+        {
+            _items.Remove(item);
+        }
+
+        public void Save()
+        {
+            //save is not required because items are added by add method to list
+        }
+    }
+}

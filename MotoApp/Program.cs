fd7497b5c8d652f50266b60217c8a56df0034443
Arkadiusz <szczotka.ark@gmail.com>
2023-03-16 14:41:41 +0100

    Basic comunication with user implemented.

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -4,10 +4,47 @@ using MotoApp.Data;
 using MotoApp.Repositories.Extensions;
 using MotoApp.Entities.Extensions;
 
+Console.WriteLine("App for attendance list.\n");
 var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext(), EmployeeAdded);
 employeeRepository.ItemAdded += RepositoryOnItemAdded;
 employeeRepository.ItemRemoved += RepositoryOnItemRemoved;
 
+bool appIsRunning = true;
+
+while (appIsRunning)
+{
+    Console.WriteLine("Please choose what to do:\nA - to add Employees\nM - to add Managers\nD - to delete by ID\nS - to show list\n q - to quit console");
+    var input = Console.ReadLine()?.ToLower();
+
+    switch (input)
+    {
+        case "a":
+            AddEmployees(employeeRepository);
+            break;
+
+        case "m":
+            AddManagers(employeeRepository);
+            break;
+
+        case "d":
+            var id = ReadId();
+            RemoveEntity(employeeRepository, id);
+            break;
+
+        case "s":
+            WriteToConsole(employeeRepository);
+            break;
+
+        case "q":
+            appIsRunning = false;
+            break;
+
+        default:
+            Console.Clear();
+            Console.Write("Inputed key is not valid\n");
+            break;
+    }
+}
 void RepositoryOnItemRemoved(object? sender, Employee e)
 {
     Console.WriteLine($"Employee removed - {e.FirstName} from {sender?.GetType().Name}");
@@ -18,10 +55,12 @@ void RepositoryOnItemAdded(object? sender, Employee e)
     Console.WriteLine($"Employee added - {e.FirstName} from {sender?.GetType().Name}");
 }
 
+/*
 AddManagers(employeeRepository);
 AddEmployees(employeeRepository);
 RemoveEntity(employeeRepository, 4);
 WriteToConsole(employeeRepository);
+*/
 
 static void EmployeeAdded(Employee item)
 {
@@ -66,5 +105,23 @@ static void RemoveEntity(SqlRepository<Employee> repository, int id)
     else
     {
         Console.WriteLine("There is no item to remove");
+    }
+}
+
+static int ReadId()
+{
+    Console.Clear();
+    Console.WriteLine("Please input numeric ID:");
+    var input = Console.ReadLine();
+
+    if (int.TryParse(input, out int id))
+    {
+        return id;
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Invalid input! 0 id returned");
+        return 0;
     }
 }
